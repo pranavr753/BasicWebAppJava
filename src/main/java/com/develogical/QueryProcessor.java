34c8e53db9e9681a27bea7b0dafd52be56432927
pranavr753 <62114276+pranavr753@users.noreply.github.com>
2024-12-02 17:03:21 +0000

    fix

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -151,4 +151,23 @@ public class QueryProcessor {
     private boolean checkIfNumberIsSquare(int number) {
         return Math.sqrt(number) % 1 == 0;
     }
+
+    private boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        if (num == 2) {
+            return true;
+        }
+        if (num % 2 == 0) {
+            return false;
+        }
+        int sqrt = (int) Math.sqrt(num);
+        for (int i = 3; i <= sqrt; i += 2) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
