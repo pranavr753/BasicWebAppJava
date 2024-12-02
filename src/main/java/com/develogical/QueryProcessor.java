0dc1864ad6bff9a5a40a37ac46faec3e3b3eda76
Nikhil Londhe <nlondhe@andrew.cmu.edu.com>
2024-12-02 16:55:59 +0000

    Fix

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -1,7 +1,11 @@
 package com.develogical;
 
-import java.util.regex.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class QueryProcessor {
 
@@ -62,8 +66,37 @@ public class QueryProcessor {
                 return String.valueOf(numbers.get(0) * numbers.get(1));
             }
         }
+        if (query.contains("Which of the following numbers is both a square and a cube:")) {
+            // Which of the following numbers is both a square and a cube: 2416, 1936, 3160, 4508, 4290, 2744, 1?
+            Pattern pattern = Pattern.compile("\\d+");
+            Matcher matcher = pattern.matcher(query);
+            List<Integer> numbers = new ArrayList<>();
+            
+            while (matcher.find()) {
+                numbers.add(Integer.parseInt(matcher.group()));
+            }
+    
+            List<Integer> validNumbers = new ArrayList<>();
+            for (int number : numbers) {
+                if (checkIfNumberIsCube(number) && checkIfNumberIsSquare(number)) {
+                    validNumbers.add(number);
+                }
+            }
+    
+            if (!validNumbers.isEmpty()) {
+                return validNumbers.stream().map(String::valueOf).collect(Collectors.joining(","));
+            }
+        }
 
         return "";
     }
 
+    
+    private boolean checkIfNumberIsCube(int number) {
+        return Math.cbrt(number) % 1 == 0;
+    }
+
+    private boolean checkIfNumberIsSquare(int number) {
+        return Math.sqrt(number) % 1 == 0;
+    }
 }
